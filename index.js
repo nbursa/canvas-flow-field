0f389316f2c9ca81c1c7a5ae1fbad9bbdf881706
Nenad Bursać <nbursa@gmail.com>
2022-10-09 21:50:05 +0200

    create grid of lines

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ window.onload = function() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height);
-    flowField.animate();
+    flowField.animate(0);
 }
 
 window.addEventListener('resize', function() {
@@ -17,7 +17,7 @@ window.addEventListener('resize', function() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height);
-    flowField.animate();
+    flowField.animate(0);
 })
 
 const mouse = {
@@ -38,24 +38,53 @@ class FlowFieldEffect {
     constructor(ctx, width, height) {
         this.#ctx = ctx;
         this.#ctx.strokeStyle = '#ffffff';
-        this.#ctx.lineWidth = 5;
+        this.#ctx.lineWidth = 1;
         this.#width = width;
         this.#height = height;
-        this.angle = 0;
+        this.lastTime = 0;
+        this.interval = 1000 / 60;
+        this.timer = 0;
+        this.cellSize = 15;
+        this.gradient;
+        this.#createGradient();
+        this.#ctx.strokeStyle = this.gradient;
     }
 
-    #draw(x, y) {
-        const length = 400;
+    #createGradient() {
+        this.gradient = this.#ctx.createLinearGradient(0, 0, this.#width, this.#height);
+        this.gradient.addColorStop(.1, '#ff5c33');
+        this.gradient.addColorStop(.2, '#ff66b3');
+        this.gradient.addColorStop(.4, '#ccccff');
+        this.gradient.addColorStop(.6, '#b3ffff');
+        this.gradient.addColorStop(.8, '#80ff80');
+        this.gradient.addColorStop(.9, '#ffff33');
+        
+    }
+
+    #drawLine(angle, x, y) {
+        const length = 200;
         this.#ctx.beginPath();
         this.#ctx.moveTo(x, y);
-        this.#ctx.lineTo(mouse.x, mouse.y);
+        this.#ctx.lineTo(x + angle * 100, y + angle * 100);
         this.#ctx.stroke();
     }
 
-    animate() {
-        this.angle += .1;
-        // this.#ctx.clearRect(0, 0, this.#width, this.#height);
-        this.#draw(this.#width / 2, this.#height / 2);
+    animate(timeStamp) {
+        const deltaTime = timeStamp - this.lastTime;
+        this.lastTime = timeStamp;
+        if(this.timer > this.interval) {
+            this.#ctx.clearRect(0, 0, this.#width, this.#height);
+            for(let y = 0; y < this.#height; y += this.cellSize) {
+                for(let x = 0; x < this.#width; x += this.cellSize) {
+                    const angle = Math.cos(x) + Math.sin(y);
+                    this.#drawLine(angle, x, y);
+                }
+            }
+            this.timer = 0;
+        } else {
+            this.timer += deltaTime;
+        }
+        // console.log(deltaTime);
         // call animate fn every frame
         flowFieldAnimation = requestAnimationFrame(this.animate.bind(this));
     }
