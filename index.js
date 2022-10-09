7ff183e57aef3a44c09d3e8b729a13688db8c9e5
Nenad Bursać <nbursa@gmail.com>
2022-10-09 21:11:27 +0200

    animate line with math

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ window.onload = function() {
 }
 
 window.addEventListener('resize', function() {
+    cancelAnimationFrame(flowFieldAnimation);
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height);
@@ -29,8 +30,7 @@ class FlowFieldEffect {
         this.#ctx.strokeStyle = '#ffffff';
         this.#width = width;
         this.#height = height;
-        this.x = 0;
-        this.y = 0;
+        this.angle = 0;
     }
 
     #draw(x, y) {
@@ -42,10 +42,9 @@ class FlowFieldEffect {
     }
 
     animate() {
+        this.angle += .1;
         this.#ctx.clearRect(0, 0, this.#width, this.#height);
-        this.#draw(this.x, this.y);
-        this.x += 2;
-        this.y += .5;
+        this.#draw(this.#width / 2 + Math.sin(this.angle) * 100, this.#height / 2 + Math.cos(this.angle) * 100);
         // call animate fn every frame
         flowFieldAnimation = requestAnimationFrame(this.animate.bind(this));
     }
