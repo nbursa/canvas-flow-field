77a08ae3ad45823d049a93b6d99be0ba6225fcf1
Nenad Bursać <nenad@Nenads-MacBook-Air.local>
2022-10-09 20:11:02 +0200

    draw line

--- index.js
+++ index.js
@@ -3,10 +3,27 @@ window.onload = function() {
     const ctx = canvas.getContext('2d');
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+    const flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height);
 }
 
 class FlowFieldEffect {
     #ctx;
     #width;
     #height;
+
+    constructor(ctx, width, height) {
+        this.#ctx = ctx;
+        this.#ctx.strokeStyle = '#ffffff';
+        this.#width = width;
+        this.#height = height;
+        this.#draw(10, 10);
+    }
+
+    #draw(x, y) {
+        const length = 400;
+        this.#ctx.beginPath();
+        this.#ctx.moveTo(x, y);
+        this.#ctx.lineTo(x + length, y + length);
+        this.#ctx.stroke();
+    }
 }
